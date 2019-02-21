45483cb290cf17e28ede0c852d3febb9037ee8c6
Bitfroest <fabian.schurig.94@gmail.com>
2019-02-22 00:22:33 +0100

    change to current running python

--- ContentCenterLoader.py
+++ ContentCenterLoader.py
@@ -44,7 +44,7 @@ def install(path, requirementsFileName):
     else:
         with open(requirementsFileName) as f:
             for line in f:
-                subprocess.check_call(['python','-m','pip','install', '-U', line, '-t', path, '--ignore-installed', '-q'])
+                subprocess.check_call([sys.executable,'-m','pip','install', '-U', line, '-t', path, '--ignore-installed', '-q'])
 
 
 def update(context):
